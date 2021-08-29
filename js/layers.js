1cf3f6ee4e43ea726be4be891e12f8455c48e9b1
ross128b <89728325+ross128b@users.noreply.github.com>
2021-08-30 01:04:37 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -7,12 +7,12 @@ addLayer("layer1", {
         unlocked: true,
 	points: new ExpantaNum(0),
     }},
-    color: "lime",
-    requires: new omeganum(10),
+    color: "#4BDC13",
+    requires: new omeganum(0),
     resource: "test point2", // Name of prestige currency
     base resource:"test point1"，
     baseAmount() {return player.points}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new ExpantaNum(1)
