7c49efc2cc8c7adc76ce1584b136366126cddfd8
ross128b <89728325+ross128b@users.noreply.github.com>
2021-08-30 00:53:58 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,19 @@
-addLayer("???", {
-    name: "???", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "???", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("layer1", {
+    name: "layer1", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "l1", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         //part1
         unlocked: true,
-		points: new ExpantaNum(0),
+	points: new ExpantaNum(0),
     }},
     color: "lime",
-    resource: "???", // Name of prestige currency
-    type: "none", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    requires: new omeganum(10),
+    resource: "test point2", // Name of prestige currency
+    base resource:"test point1"
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new ExpantaNum(1)
         return mult
@@ -17,7 +21,7 @@ addLayer("???", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new ExpantaNum(1)
     },
-    row: 1, // Row the layer is in on the tree (0 is the first row)  QwQ:1也可以当第一排
+    row: 0, // Row the layer is in on the tree (0 is the first row)  QwQ:1也可以当第一排
     //layerShown(){return player.v.total.gte(1)},
     //clickables: {
         //part1
