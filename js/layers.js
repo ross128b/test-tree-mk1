3fc805f8d0395ed257193b06233cf49c1bb03f38
ross128b <89728325+ross128b@users.noreply.github.com>
2021-09-07 12:06:31 +0800

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -14,19 +14,23 @@ addLayer("layer1", {
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
+    softcap: new OmegaNum(1e100), 
+    softcapPower: new OmegaNum(0.5), 
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new ExpantaNum(1)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new ExpantaNum(1)
+        let gexp = new ExpantaNum(1)
+        if(hasUpgrade(this.layer, 14))gexp=gexp.add(100)
+        return gexp
     },
     row: 1, // Row the layer is in on the tree (0 is the first row)  QwQ:1也可以当第一排
     upgrades:{
         11: {
             title: "upgrade1",
             description: "no effect",
-            cost: new OmegaNum(1),
+            cost: new OmegaNum(0),
             unlocked() { return player[this.layer].unlocked },
         },
         12: {
@@ -41,10 +45,17 @@ addLayer("layer1", {
             cost: new OmegaNum(3),
             unlocked() { return (hasUpgrade(this.layer,12))},
             effect() {
-                var eff=0
-                eff = eff * "testPoint2"
+                var eff=1
+                eff = player[this.layer].points.add(1).pow(0.5)
                 return eff;
             },
+            effectDisplay() { return format(upgradeEffect("layer1",13))+"x" },
         },
+        14: {
+            title: "more testPoint1!",
+            description: "testPoint2 ^100",
+            cost: new OmegaNum(10),
+            unlocked() { return (hasUpgrade(this.layer,13))},
+        }
     }
 })
